e43cf03b36318dbc6778e00fc75a35b5a38b67e2
Ben Muchiri <bernard.njoroge@strathmore.edu>
2020-05-03 15:00:02 -0700

    Get current date and time

--- auto-zoom.cpp
+++ auto-zoom.cpp
@@ -3,8 +3,6 @@
 #include <string>
 #include <ctime>
 
-#define CLOCKS_PER_SEC 1000;
-
 using namespace std;
 
 char *linkChar = "https://strathmore.zoom.us/j/91663632505?pwd=eHBuamhFQkhxK0hwVXNwVGROc1E2QT09";
@@ -19,10 +17,27 @@ void sleep(float seconds)
     return;
 }
 
+tm *getTime()
+{
+    // current date/time based on current system
+    time_t now = time(0);
+
+    return localtime(&now);
+}
+
 int main()
 {
+    //get current time and day
+    tm *ltm = getTime();
+    int hour = ltm->tm_hour;
+    int min = ltm->tm_min;
+    int day = ltm->tm_wday;
+
     //open link in browser(internet explorer)
     ShellExecute(NULL, "open", "iexplore", linkChar, NULL, SW_SHOWNORMAL);
-    sleep(7);
+
+    //sleep for 10sec
+    sleep(10);
+    //close browser(internet explorer)
     system("taskkill /F /T /IM iexplore.exe");
 }
