b96e655ea7c894a34a2a2531e6cdbd2bcb0bacdc
Ben Muchiri <bernard.njoroge@strathmore.edu>
2020-05-03 14:33:45 -0700

    Close browser after few seconds

--- auto-zoom.cpp
+++ auto-zoom.cpp
@@ -1,13 +1,28 @@
 #include <windows.h>
 #include <iostream>
 #include <string>
+#include <ctime>
+
+#define CLOCKS_PER_SEC 1000;
 
 using namespace std;
 
 char *linkChar = "https://strathmore.zoom.us/j/91663632505?pwd=eHBuamhFQkhxK0hwVXNwVGROc1E2QT09";
 
+void sleep(float seconds)
+{
+    clock_t startClock = clock();
+    float secondsAhead = seconds * CLOCKS_PER_SEC;
+    // do nothing until the elapsed time has passed.
+    while (clock() < startClock + secondsAhead)
+        ;
+    return;
+}
+
 int main()
 {
     //open link in browser(internet explorer)
     ShellExecute(NULL, "open", "iexplore", linkChar, NULL, SW_SHOWNORMAL);
+    sleep(7);
+    system("taskkill /F /T /IM iexplore.exe");
 }
