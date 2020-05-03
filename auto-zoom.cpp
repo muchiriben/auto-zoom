daaba6862ae717d1ea69a17a70c7cd437e6a9c40
Ben Muchiri <bernard.njoroge@strathmore.edu>
2020-05-03 15:36:33 -0700

    set timetable

--- auto-zoom.cpp
+++ auto-zoom.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-char *linkChar = "https://strathmore.zoom.us/j/91663632505?pwd=eHBuamhFQkhxK0hwVXNwVGROc1E2QT09";
+char *linkChar = "https://strathmore.zoom.us/j/91663632505";
 
 void sleep(float seconds)
 {
@@ -25,6 +25,104 @@ tm *getTime()
     return localtime(&now);
 }
 
+void openClass(int hour, int min, int day)
+{
+    //monday class
+    if (day == 1)
+    {
+        if (hour == 8 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+
+        if (hour == 11 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+
+        if (hour == 15 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+    }
+
+    //tuesday class
+    if (day == 2)
+    {
+        if (hour == 8 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+
+        if (hour == 11 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+    }
+
+    //wednesday class
+    if (day == 3)
+    {
+        if (hour == 8 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+
+        if (hour == 10 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+
+        if (hour == 13 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+
+        if (hour == 15 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+    }
+
+    //thursday class
+    if (day == 4)
+    {
+        if (hour == 8 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+
+        if (hour == 12 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+
+        if (hour == 15 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+    }
+
+    //friday class
+    if (day == 5)
+    {
+        if (hour == 8 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+
+        if (hour == 11 && min == 15)
+        {
+            ShellExecute(NULL, "open", "iexplore", "https://strathmore.zoom.us/j/91663632505", NULL, SW_SHOWNORMAL);
+        }
+    }
+
+    //sleep for 10sec
+    sleep(10);
+    //close browser(internet explorer)
+    system("taskkill /F /T /IM iexplore.exe");
+}
+
 int main()
 {
     //get current time and day
@@ -34,10 +132,5 @@ int main()
     int day = ltm->tm_wday;
 
     //open link in browser(internet explorer)
-    ShellExecute(NULL, "open", "iexplore", linkChar, NULL, SW_SHOWNORMAL);
-
-    //sleep for 10sec
-    sleep(10);
-    //close browser(internet explorer)
-    system("taskkill /F /T /IM iexplore.exe");
+    openClass(hour, min, day);
 }
